bbb421b41c5f2827d9f81bb47c49b492359f97fd
Caio Harrys <blackiaratanmusic@gmail.com>
2026-08-05 23:55:41 -0300

    Adição de Banners das bebidas novas

--- src/assets/Componentes/BannersSection.jsx
+++ src/assets/Componentes/BannersSection.jsx
@@ -3,10 +3,18 @@ import banner_gingibre from "../Imagens/Banners/Banner_Site_Page/banner_gingibre
 import banner_rubra from "../Imagens/Banners/Banner_Site_Page/banner_rubra.jpg";
 import banner_veneta from "../Imagens/Banners/Banner_Site_Page/banner_veneta.jpg";
 import banner_moderacao from "../Imagens/Banners/Banner_Site_Page/banner_moderacao.png";
+import banner_xeque from "../Imagens/Banners/Banner_Site_Page/XequeBanner.png";
+import banner_muruja from "../Imagens/Banners/Banner_Site_Page/MascateMuruja.png";
+import banner_melan from "../Imagens/Banners/Banner_Site_Page/MascateMelanciaBanner.png";
+import banner_blau from "../Imagens/Banners/Banner_Site_Page/ChablauBanner.png";
 import "../Css/BannersSection.css";
 
 export default function BannersSection() {
   const banners = [
+    { image: banner_xeque, alt: "Banner Xeque-Mate" },
+    { image: banner_muruja, alt: "Banner Mascate Muruja" },
+    { image: banner_melan, alt: "Banner Mascate Melancia" },
+    { image: banner_blau, alt: "Banner Chablauzin" },
     { image: banner_gingibre, alt: "Banner Gingibre" },
     { image: banner_rubra, alt: "Banner Rubra" },
     { image: banner_veneta, alt: "Banner Veneta" },
